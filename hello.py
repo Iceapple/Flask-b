0bb9ea4d5d20ebd923496e0ced3ead2487b1bb49
Iceapple <flyvcc@gmail.com>
2016-11-17 12:15:33 +0800

    ok

--- hello.py
+++ hello.py
@@ -1,19 +1,32 @@
 # _*_ coding: utf-8 _*_
-from flask import Flask
+"""
+model:
+	Flask
+	request
+	make_response
+	Flask-Script -> 命令行解析
+		Manager类
+"""
+from flask import Flask, request, make_response
+from flask.ext.script import Manager
 
 app = Flask(__name__)
+manager = Manager(app)
 
 @app.route('/')
 def index():
-	return '<h1>Hello, World!</h1>'
+	# 一个上下文的例子(request)
+	# user_agent = request.headers.get('User_Agent')
+	# return '<p>Your browers is %s!</p>' % user_agent
+	response = make_response('<h1>This documents carries a cookie!</h1>')
+	response.set_cookie('answer', '42')
+	return '<h1>Hello, World!</h1>', 200
 
-@app.route('/user/<name>')
-def user(name):
-	return '<h1>Hello, %s!</h1>' % name
+@app.route('/user/<id>')
+def user(id):
+	return '<1>Hello, %s!</h1>' % id
 
 if __name__ == '__main__':
-	app.run(debug=True)
-
-
-
-	### eeee
+	# app.run(debug=True)
+	# 命令行方式输入参数 runserver --help
+	manager.run()
